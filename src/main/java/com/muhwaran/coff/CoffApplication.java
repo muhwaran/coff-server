e467808d4e5a2f8d140658a853bb9956d35e1d42
idealflower-k <aekrcu@naver.com>
2024-09-02 08:53:12 +0900

    feat: init spring project

---
+++ src/main/java/com/muhwaran/coff/CoffApplication.java
@@ -0,0 +1,13 @@
+package com.muhwaran.coff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffApplication.class, args);
+	}
+
+}
